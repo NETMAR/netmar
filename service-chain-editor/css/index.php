ad67d50031cb2441f31c760a8fdfd0372bcff722
Richard Whitfield <riwh@pmpc1310.npm.ac.uk>
2012-10-30 11:18:22 +0000

    Updated CSS structure

---
+++ service-chain-editor/css/index.php
@@ -0,0 +1,12 @@
+<?php
+
+	header('Content-type: text/css');
+echo file_get_contents('jquery-ui.css') . "\n\n";
+echo file_get_contents('jquery.ui.slider.css') . "\n\n";
+echo file_get_contents('jquery.context-menu.css') . "\n\n";
+echo file_get_contents('panel.css') . "\n\n";
+echo file_get_contents('services.css') . "\n\n";
+echo file_get_contents('dialogBox.css') . "\n\n";
+echo file_get_contents('workspace.css') . "\n\n";
+
+?>
